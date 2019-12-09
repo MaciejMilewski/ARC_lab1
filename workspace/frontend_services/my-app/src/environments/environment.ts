26fe5e568a69ea6bb8d2fc3c8dad1acaf5ca980e
Damian Zarzycki <36889073+DamianZarzycki@users.noreply.github.com>
2019-12-09 21:39:06 +0100

    update

--- workspace/frontend_services/my-app/src/environments/environment.ts
+++ workspace/frontend_services/my-app/src/environments/environment.ts
@@ -6,13 +6,7 @@ export const environment = {
   production: false,
   
   firebaseConfig: {
-    apiKey: "AIzaSyCi6uU0Xmzip7j6XRkgwbUERx4F9TeJePQ",
-    authDomain: "arc-pjatk.firebaseapp.com",
-    databaseURL: "https://arc-pjatk.firebaseio.com",
-    projectId: "arc-pjatk",
-    storageBucket: "arc-pjatk.appspot.com",
-    messagingSenderId: "667651019600",
-    appId: "1:667651019600:web:e87d7db2ce55e180bc9255"
+    
   }
   
 };
